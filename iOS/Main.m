a7dd00ba85ce8bff7f02638245baa9c25e8d8c9a
kodawxxx <vanillah.vanilla@gmail.com>
2025-12-04 22:47:23 +0100

    Update Main.m

--- iOS/Main.m
+++ iOS/Main.m
@@ -3,7 +3,7 @@
 #import <Foundation/Foundation.h>
 #import <objc/runtime.h>
 
-#define API_URL @"http://26.101.113.15:6532"
+#define API_URL @"http://26.101.113.15:"
 #define EPIC_GAMES_URL @"ol.epicgames.com"
 
 @interface CustomURLProtocol : NSURLProtocol
